dd12374dd273ea94bf58f08b3b2f003e5edd6c76
Paige Nelmes <designer@paigenelmes.com>
2023-05-16 13:54:04 -0700

    Add Libraries/Frameworks Skills

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -7,21 +7,20 @@ export const Skills = () => {
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
         <h2 className="py-4">Skills</h2>
         {/* SKILL SUBHEADER: LANGUAGES */}
-        <p className="uppercase tracking-widest text-[#5C6CFF] py-4">
+        <p className="uppercase tracking-widest text-[#5C6CFF] py-6">
           Languages
         </p>
-        {/* GRID SETTINGS */}
+        {/* GRID SETTINGS FOR LANGUAGES */}
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
-
           {/* JAVASCRIPT SKILL */}
-          <div className="p-6 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
                   src="/../public/skills/javascript-logo.png"
                   alt="/"
-                  width="64"
-                  height="64"
+                  width="48"
+                  height="48"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -31,14 +30,14 @@ export const Skills = () => {
           </div>
 
           {/* HTML SKILL */}
-          <div className="p-6 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
                   src="/../public/skills/html-logo.png"
                   alt="/"
-                  width="64"
-                  height="64"
+                  width="48"
+                  height="48"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -54,8 +53,8 @@ export const Skills = () => {
                 <Image
                   src="/../public/skills/css-logo.png"
                   alt="/"
-                  width="45"
-                  height="45"
+                  width="40"
+                  height="40"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
@@ -65,22 +64,22 @@ export const Skills = () => {
           </div>
 
           {/* RUBY SKILL */}
-          <div className="p-6 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
               <div className="m-auto">
                 <Image
                   src="/../public/skills/ruby-logo.png"
                   alt="/"
-                  width="64"
-                  height="64"
+                  width="48"
+                  height="48"
                 />
               </div>
               <div className="flex flex-col items-center justify-center">
-                <h3 className="uppercase">RUBY</h3>
+                <h3 className="uppercase">Ruby</h3>
               </div>
             </div>
           </div>
-          
+
           {/* SQL SKILL */}
           <div className="p-10 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
             <div className="grid grid-cols-2 gap-4 justify-center items-center">
@@ -97,7 +96,189 @@ export const Skills = () => {
               </div>
             </div>
           </div>
-
+        </div>
+        {/* SKILL SUBHEADER: LIBRARIES/FRAMEWORKS */}
+        <p className="uppercase tracking-widest text-[#5C6CFF] py-6">
+          Libraries &amp; Frameworks
+        </p>
+        {/* GRID SETTINGS FOR LIBRARIES/FRAMEWORKS */}
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {/* REACT SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/react-logo.png"
+                  alt="/"
+                  width="48"
+                  height="48"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">React</h3>
+              </div>
+            </div>
+          </div>
+          {/* NEXTJS SKILL */}
+          <div className="p-10 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/nextjs-logo.png"
+                  alt="/"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">NextJS</h3>
+              </div>
+            </div>
+          </div>
+          {/* NODEJS SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#ECA0FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/node-js-logo.png"
+                  alt="/"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">NodeJS</h3>
+              </div>
+            </div>
+          </div>
+          {/* TAILWIND SKILL */}
+          <div className="p-7 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/tailwind-logo.png"
+                  alt="/"
+                  width="48"
+                  height="48"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">Tailwind CSS</h3>
+              </div>
+            </div>
+          </div>
+          {/* MUI SKILL */}
+          <div className="p-7 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/mui-logo.png"
+                  alt="/"
+                  width="48"
+                  height="48"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">MUI</h3>
+              </div>
+            </div>
+          </div>
+          {/* RAILS SKILL */}
+          <div className="p-9 shadow-md rounded-xl border-2 border-[#ECA0FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/rails-logo.png"
+                  alt="/"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">Rails</h3>
+              </div>
+            </div>
+          </div>
+          {/* EXPRESS SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/express-logo.png"
+                  alt="/"
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">Express</h3>
+              </div>
+            </div>
+          </div>
+          {/* AJAX SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/ajax-logo.png"
+                  alt="/"
+                  width="64"
+                  height="64"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">Ajax</h3>
+              </div>
+            </div>
+          </div>
+          {/* EJS SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#ECA0FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/ejs-logo.png"
+                  alt="/"
+                  width="34"
+                  height="34"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">EJS</h3>
+              </div>
+            </div>
+          </div>
+             {/* AXIOS SKILL */}
+             <div className="p-8 shadow-md rounded-xl border-2 border-[#84FFC9] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/axios-logo.png"
+                  alt="/"
+                  width="100"
+                  height="100"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">Axios</h3>
+              </div>
+            </div>
+          </div>
+          {/* JQUERY SKILL */}
+          <div className="p-8 shadow-md rounded-xl border-2 border-[#AAB2FF] hover:scale-110 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src="/../public/skills/jquery-logo.png"
+                  alt="/"
+                  width="90"
+                  height="90"
+                />
+              </div>
+              <div className="flex flex-col items-center justify-center">
+                <h3 className="uppercase">JQuery</h3>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
