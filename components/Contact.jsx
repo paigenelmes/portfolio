c211c8d5088f61f33ef051db98c83020d24e0293
Paige Nelmes <designer@paigenelmes.com>
2023-05-18 14:13:19 -0700

    Add icons to left side of contact info

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import { AiOutlineMail } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsFillPersonLinesFill } from "react-icons/bs";
 
 function Contact() {
   return (
@@ -12,7 +15,7 @@ function Contact() {
         {/* CONTACT SECTION GRID */}
         <div className="grid lg:grid-cols-5 gap-10">
           {/* LEFT SIDE */}
-          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-md shadow-gray-400 rounded-xl p-2 bg-gradient-to-t from-[#84FFC9] via-[#AAB2FF] to-[#ECA0FF]">
+          <div className="col-span-3 lg:col-span-2 w-full h-full shadow-md shadow-gray-400 rounded-xl p-2 bg-white">
             <div className="lg:p-2 h-full">
               <div>
                 <Image
@@ -23,9 +26,36 @@ function Contact() {
                   alt="Paige Cartoon Pic"
                 ></Image>
               </div>
+              <div>
+                <h2 className="pt-4 pb-2 text-[#363537] text-center">Paige Nelmes</h2>
+                <h4 className="text-[#5C6CFF] uppercase tracking-wider text-center">
+                  Full Stack Developer
+                </h4>
+                <p className="text-[#363537] text-center py-4">
+                  Open to freelance or full-time opportunities. <br /> Contact me and let&apos;s chat!
+                </p>
+              </div>
+              <div>
+              <div>
+                <div className="flex items-center justify-evenly m-auto py-4">
+                  <div className="rounded-full shadow-md shadow-gray-400 p-4 cursor-pointer bg-gradient-to-t from-[#84FFC9] via-[#AAB2FF] to-[#ECA0FF] hover:scale-150 ease-in duration-300">
+                    <FaLinkedinIn className="fill-white scale-125"></FaLinkedinIn>
+                  </div>
+                  <div className="rounded-full shadow-md shadow-gray-400 p-4 cursor-pointer bg-gradient-to-t from-[#84FFC9] via-[#AAB2FF] to-[#ECA0FF] hover:scale-150 ease-in duration-300">
+                    <FaGithub className="fill-white scale-125"></FaGithub>
+                  </div>
+                  <div className="rounded-full shadow-md shadow-gray-400 p-4 cursor-pointer bg-gradient-to-t from-[#84FFC9] via-[#AAB2FF] to-[#ECA0FF] hover:scale-150 ease-in duration-300">
+                    <AiOutlineMail className="fill-white scale-125"></AiOutlineMail>
+                  </div>
+                  <div className="rounded-full shadow-md shadow-gray-400 p-4 cursor-pointer bg-gradient-to-t from-[#84FFC9] via-[#AAB2FF] to-[#ECA0FF] hover:scale-150 ease-in duration-300">
+                    <BsFillPersonLinesFill className="fill-white scale-125"></BsFillPersonLinesFill>
+                  </div>
+                </div>
             </div>
+              </div>
+            </div>
+            {/* RIGHT SIDE */}
           </div>
-          {/* RIGHT SIDE */}
         </div>
       </div>
     </div>
