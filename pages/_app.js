ab582ad6f1f195a92809757f89edd193ada24d7d
Paige Nelmes <designer@paigenelmes.com>
2023-05-23 15:22:02 -0700

    Change font to Merriweather Sans

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Merriweather_Sans } from 'next/font/google';
+const merryweatherSans = Merriweather_Sans({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={merryweatherSans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,8 @@ import { Skills } from '@/components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Design from '@/components/Design';
-import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
